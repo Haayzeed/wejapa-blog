fa4fca1fe230d775163c7e54bda4a9794cc01237
Dosunmu Afeez <dosunmuafeez37@gmail.com>
2020-08-12 18:46:10 +0100

    new commit

--- src/components/blogs.vue
+++ src/components/blogs.vue
@@ -1,9 +1,10 @@
 <template>
     <div>
-        <div class="container">
+        <div class="container pb-5">
             <h3 class="text-center font-weight-bold">Welcome to Wejapa Blog</h3>
             <hr class="d-block mx-auto mb-4" style="width: 10%; border: 2px solid black">
-            <div class="col-md-12 mb-4" v-for="(blog, index) in blogs" :key="index">
+            <paginate name="blogs" :list="blogs" :per="10">
+            <div class="col-md-12 mb-4" v-for="(blog, index) in paginated('blogs')" :key="index">
                 <div class="card">
                     <div class="card-body">
                         <div class="row">
@@ -18,6 +19,16 @@
                     </div>
                 </div>
             </div>
+            </paginate>
+            <!-- pagination -->
+            <div class="col-md-12">
+                <div class="pagination text-center">
+                    <paginate-links for="blogs" :async="true" :show-step-links="true" :step-links="{
+                        next: 'Next',
+                        prev: 'Prev' }" class="page-links pagination text-center">
+                    </paginate-links>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -27,8 +38,15 @@ export default {
     data(){
         return{
             id: this.$route.params.id,
-            blogs: []
+            blogs: [],
+            paginate: ['blogs']
         }
+    },
+    methods:{
+        // clickCallback: function(page) {
+        // console.log(page)
+    
+    // }
     },
     created(){
     axios.get('https://jsonplaceholder.typicode.com/posts').then((response) =>{
@@ -36,4 +54,26 @@ export default {
     })
   }
 }
+// :prev-text="prev"
+                // :next-text="next"
 </script>
+<style>
+.pagination{
+    margin: 0 auto !important;
+}
+.page-links{
+    text-align: center !important;
+}
+    .pagination .left-arrow a, .pagination .right-arrow a{
+        background: #041705 !important;
+        color: #fff !important;
+        padding: 8px 10px !important;
+    }
+    .pagination .right-arrow a{
+        margin-left: 5px;
+    }
+    .number a{
+        color: #041705 !important;
+        margin-left: 5px
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 import App from './App.vue'
 import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router'
+import VuePaginate from 'vue-paginate'
 import blogs from './components/blogs'
 import viewBlogs from './components/viewBlogs'
 import '@/assets/css/bootstrap.css'
@@ -11,6 +12,7 @@ import '@/assets/css/style.css'
 
 Vue.use(VueRouter)
 Vue.use(VueAxios, axios)
+Vue.use(VuePaginate)
 
 // const axios = require('axios');
 const routes = [
